a300778d0c82e7c602e71845801d82c023814366
urmallampati <urmallampati@Udayarathalek-Mallampati-MBP.local>
2018-03-21 12:10:32 -0500

    Added few challenges

---
+++ jsArrayPlay.js
@@ -0,0 +1,10 @@
+<html>
+<script>
+var jsObj =  {
+    "name":"JavaScript",
+    "Description":"I am excellent!",
+    "quality":"intuitive",
+    "learn-methodology":"Debug and practice"
+};
+</script>
+</html>
